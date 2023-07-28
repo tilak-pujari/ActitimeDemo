9ab4c5bcf59a2ff754bc032a4fdfa24cdd41d5e9
tilak <tilak@LAPTOP-2122UIPD>
2023-07-28 17:29:42 +0530

    check if Number is Armstrong Number or not

---
+++ src/programs/P07_ArmStrongNum.java
@@ -0,0 +1,36 @@
+package programs;
+import java.util.Scanner;
+
+public class P07_ArmStrongNum {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter a number: ");
+		int num = sc.nextInt();
+		
+		int digits = 0;// Get the number of digits in num
+		int temp = num;
+		while (temp > 0) {
+			digits++;
+			temp = temp / 10;
+		}
+		int sum = 0;
+		temp = num;
+		while (temp > 0) {
+			int digit = temp % 10;
+
+			int power = 1;// Use a loop to calculate the power of digit
+			for (int i = 0; i < digits; i++) {
+				power = power * digit;
+			}
+			sum = sum + power;
+			temp = temp / 10;
+		}
+
+		if (sum == num) { // Check if the sum is equal to num and print the result
+			System.out.println(num + " is an Armstrong Number.");
+		} else {
+			System.out.println(num + " is not Armstrong Number.");
+		}
+	}
+}
