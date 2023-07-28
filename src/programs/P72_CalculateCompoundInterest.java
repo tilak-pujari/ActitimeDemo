3cf4106cbbb5c3b4a816720b43dc67f968ca60d9
tilak <tilak@LAPTOP-2122UIPD>
2023-07-28 17:27:23 +0530

    Calculate Simple and Compound Interest

---
+++ src/programs/P72_CalculateCompoundInterest.java
@@ -0,0 +1,23 @@
+package programs;
+
+import java.util.Scanner;
+
+public class P72_CalculateCompoundInterest {
+
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the principle amount");
+		int principle = sc.nextInt();
+		System.out.println("Enter the rate of interest");
+		double rate = sc.nextDouble();
+		System.out.println("Enter the time year");
+		double time = sc.nextDouble();
+		
+		//double ci=principle*(1+(rate/100))^time-principle;
+		double b=(1+(rate/100));
+		double ci=principle*Math.pow(b, time);
+		System.out.println(ci);
+	}
+
+}
