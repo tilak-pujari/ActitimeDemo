d8ec70bb2bfb55764116ed1f1cc919e24356bf2b
Tilak <tilakakumarp@gmail.com>
2023-08-08 15:08:34 +0530

    mirrored right angle triangle pattern program

---
+++ src/patternPrograms/P06_Mirror_Right_Angle_Triangle.java
@@ -0,0 +1,19 @@
+package patternPrograms;
+
+public class P06_Mirror_Right_Angle_Triangle {
+
+	public static void main(String[] args) {
+		int n = 7;
+//inner loop  
+		for (int i = 0; i <= n; i++) {
+//outer loop  
+			for (int j = 1; j <= n - i; j++) {
+				System.out.print(" ");
+			}
+			for (int k = 0; k <= i; k++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
