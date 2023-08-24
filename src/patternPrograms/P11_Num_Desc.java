aed1a725fd651fed6655798c10681f27905b0924
Tilak <tilakakumarp@gmail.com>
2023-08-24 16:57:48 +0530

    descending number pattern

---
+++ src/patternPrograms/P11_Num_Desc.java
@@ -0,0 +1,17 @@
+package patternPrograms;
+
+public class P11_Num_Desc {
+
+	public static void main(String[] args) {
+		{
+			int i, j;
+			int n = 5;
+			for (i = n; i > 0; i--) {
+				for (j = 1; j <= i; j++) {
+					System.out.print(j);
+				}
+				System.out.println("");
+			}
+		}
+	}
+}
