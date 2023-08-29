bbb7a15b88be55456d679fa5557d2e48e5d8006b
Tilak <tilakakumarp@gmail.com>
2023-08-29 15:14:21 +0530

    java program to get area of shapes

---
+++ src/udemyPracticePrograms/ShapesArea.java
@@ -0,0 +1,60 @@
+package udemyPracticePrograms;
+
+public class ShapesArea {
+
+	public static void main(String[] args) {
+		Shape s=new Circle("circle",10.5);
+		System.out.println(s.calculateArea());
+
+	}
+
+}
+
+abstract class Shape {
+    protected String name;
+
+    public Shape(String name) {
+        this.name = name;
+    }
+
+    public abstract double calculateArea();
+
+    public void displayInfo() {
+        System.out.println("Shape: " + name);
+        System.out.println("Area: " + calculateArea());
+    }
+}
+
+// TODO: Create a Circle class that extends Shape
+// The Circle class should have a private double radius attribute and a constructor to initialize it
+// It should also implement the calculateArea() method 
+class Circle extends Shape{
+    private double radius;
+    public Circle(String name,double radius){
+        super(name);
+        this.radius=radius;
+    }
+    
+    @Override
+    public double calculateArea(){
+        return (Math.PI*radius*radius);
+    }
+}
+
+// TODO: Create a Rectangle class that extends Shape
+// The Rectangle class should have private double attributes for length and width, and a constructor to initialize them
+// It should also implement the calculateArea() method 
+class Rectangle extends Shape{
+    private double length;
+    private double width;
+    Rectangle(String name,double length,double width){
+        super(name);
+        this.length=length;
+        this.width=width;
+    }
+    
+    @Override
+    public double calculateArea(){
+        return (length*width);
+    }
+}
