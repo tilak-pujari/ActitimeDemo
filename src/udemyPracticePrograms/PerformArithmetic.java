d3b8f58dd76cea61eb3d2b92dfe17213782de5f1
Tilak <tilakakumarp@gmail.com>
2023-08-29 14:51:46 +0530

    java program to override perform arithmetic operations

---
+++ src/udemyPracticePrograms/PerformArithmetic.java
@@ -0,0 +1,57 @@
+package udemyPracticePrograms;
+
+public class PerformArithmetic {
+	public int perform(int a, int b) {
+		return a + b;
+
+	}
+
+	public static void main(String[] args) {
+
+		Add a = new Add();
+		System.out.println(a.perform(10, 5));
+
+		Sub s = new Sub();
+		System.out.println(s.perform(10, 5));
+
+		Multiply m = new Multiply();
+		System.out.println(m.perform(10, 5));
+
+		Divide d = new Divide();
+		System.out.println(d.perform(10, 5));
+		// check if denom is zero
+		System.out.println(d.perform(10, 0));
+
+	}
+}
+
+class Add extends PerformArithmetic {
+	@Override
+	public int perform(int a, int b) {
+		return a + b;
+	}
+}
+
+class Sub extends PerformArithmetic {
+	@Override
+	public int perform(int a, int b) {
+		return a - b;
+	}
+}
+
+class Multiply extends PerformArithmetic {
+	@Override
+	public int perform(int a, int b) {
+		return a * b;
+	}
+}
+
+class Divide extends PerformArithmetic {
+	@Override
+	public int perform(int a, int b) {
+		if (b == 0) {
+			return -1;
+		}
+		return a / b;
+	}
+}
